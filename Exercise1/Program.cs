581739337f2d1dcaedc340ea7955d21dc3f41794
dimas <dimas@DESKTOP-03S5099>
2022-07-17 22:06:10 +0300

    Добавьте файлы проекта.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+
+            string[] userSplitText = SplitText(Console.ReadLine()); // Вызов метода разделения строк. В качестве передавамего параметра используется введенный пользователем текст
+
+            ShowSplitText(userSplitText);
+
+            Console.ReadKey();
+
+        }
+
+        /// <summary>
+        /// Метод разделяет предложение на отдельные слова. В качестве разделителя используется смвол " ".
+        /// Возвращается массив типа string в котором содержаться слова предложения.
+        /// </summary>
+        /// <param name="text"> Предложение, которое будет разделено на слова</param>
+        /// <returns></returns>
+        static string[] SplitText(string text)
+        {
+            return text.Split(" ");
+        }
+
+
+        /// <summary>
+        /// Метод выводит каждое значение передаваемого массива на отдельной строке
+        /// </summary>
+        /// <param name="text"></param>
+        static void ShowSplitText(string[] text)
+        {
+            Console.WriteLine("Вывод каждого слова в отдельной строке:\n");
+            foreach (string t in text)
+            {
+                Console.WriteLine(t);
+            }
+        }
+    }
+}
---
+++ Exercise2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите фразу:");
+            string reverseUserText = ReverseWords(Console.ReadLine());
+            Console.WriteLine($"Инвертированная фраза:\n{reverseUserText}");
+        }
+
+
+        /// <summary>
+        /// Метод меняет местами слова во фразе, которая передается в метод.
+        /// Внутри метода вызывается метод, котрый разделяет фразу на отдельные слова
+        /// </summary>
+        /// <param name="inputPrahse">Фраза, которая будет инвертирована</param>
+        /// <returns></returns>
+        static string ReverseWords(string inputPrahse)
+        {
+            string[] splitPrahse = SplitText(inputPrahse);
+            string reversWords = "";
+            for (int i = splitPrahse.Length-1; i >= 0; i--)
+            {
+                reversWords += splitPrahse[i] + " ";
+            }
+            return reversWords;
+        }
+
+        /// <summary>
+        /// Метод разделяет предложение на отдельные слова. В качестве разделителя используется смвол " ".
+        /// Возвращается массив типа string в котором содержаться слова предложения.
+        /// </summary>
+        /// <param name="text"> Предложение, которое будет разделено на слова</param>
+        /// <returns></returns>
+        static string[] SplitText(string text)
+        {
+            return text.Split(" ");
+        }
+    }
+}
